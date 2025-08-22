c053cb056e2f32fa0c6f825b7221e0ba9a4986d3
CodeWithMeBabe <sayeedahmad06062002@gmail.com>
2025-08-22 19:52:30 +0530

    Update and rename square.cpp to number_square.cpp

--- pattern/number_square.cpp
+++ pattern/number_square.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>rows;
     for(int i=1;i<=rows;i++){
         for(int j=1;j<=rows;j++){
-            cout<<"* ";
+            cout<<rows*(i-1)+j<<" ";
         }
         cout<<endl;
     }
