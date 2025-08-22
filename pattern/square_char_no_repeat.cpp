700df73e497c32d5e2ea780b0bd008d9e297aa88
CodeWithMeBabe <sayeedahmad06062002@gmail.com>
2025-08-22 20:11:05 +0530

    Create square_char_no_repeat.cpp

---
+++ pattern/square_char_no_repeat.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows;
+    cout<<"Enter number of rows: ";
+    cin>>rows;
+    cout<<"Square pattern with characters : "<<endl;
+    for(int i=1;i<=rows;i++){
+        for(char j=65; j<=65+rows-1;j++){
+            cout<< char(rows*(i-1)+j) << " ";
+        }
+        cout<<endl;
+    }
+}
